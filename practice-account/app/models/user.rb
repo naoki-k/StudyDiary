a3d9e4fc784c2f70afa36c09c3a6d8030184382b
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-05-31 18:56:21 +0900

    メールアドレスのバリデータのgemを追加

--- practice-account/app/models/user.rb
+++ practice-account/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 250 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    'valid_email_2/email': true,
                     uniqueness: { case_sensitive: false }
 
   private
