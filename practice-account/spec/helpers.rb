d1cf385f8111035e7053290b14bc63b56b987de8
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-06-03 14:34:09 +0900

    インデントの修正

--- practice-account/spec/helpers.rb
+++ practice-account/spec/helpers.rb
@@ -1,15 +1,15 @@
 module Helpers
-    def valid_all(args)
-        args.each do |target|
-            return false unless target.valid?
-        end
-        return true
+  def valid_all(args)
+    args.each do |target|
+      return false unless target.valid?
     end
+    return true
+  end
 
-    def invalid_all(args)
-        args.each do |target|
-            return false if target.valid?
-        end
-        return true
+  def invalid_all(args)
+    args.each do |target|
+      return false if target.valid?
     end
+    return true
+  end
 end
