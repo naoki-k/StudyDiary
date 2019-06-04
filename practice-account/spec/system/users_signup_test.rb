836f63a593a7aabbdc8c0e151a9152a27f942c6b
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-06-04 16:49:37 +0900

    system testの実装

--- practice-account/spec/system/users_signup_test.rb
+++ practice-account/spec/system/users_signup_test.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
-  render_views
+RSpec.describe "SignUp", type: :system do
 
+  context "アカウント登録のとき" do
+    it :aggregate_failures do
+      visit :signup
+      fill_in 'name', with: 'hogehoge'
+      fill_in 'email', with: 'hogehoge@example.com'
+      fill_in 'password', with: 'password'
+      fill_in 'password confirmation', with: 'password'
+      click_button 'sign up'
+      expect(page.body).to have_selector 'div.alert', text: 'Account created'
+      expect(page.body).to have_link 'Sign up'
+    end
+  end
 end
