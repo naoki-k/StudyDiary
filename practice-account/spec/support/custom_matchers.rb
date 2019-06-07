97993ffeb69c059b87a2c4f4073efd5280a8beb1
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-06-07 16:18:36 +0900

    カスタム抹茶の成功時メッセージの設定

--- practice-account/spec/support/custom_matchers.rb
+++ practice-account/spec/support/custom_matchers.rb
@@ -1,29 +1,41 @@
-RSpec::Matchers.define :valid_all_values do |values|
-  match do |model|
-    return false if @column.blank?
+module CustomMatchers
+  module ValidHelper
+    RSpec::Matchers.define :valid_all_values do |values|
+      match do |model|
+        return false if @column.blank?
+    
+        values.all? do |value|
+          model[@column] = value
+          model.valid?
+        end
+      end
+    
+      chain :column do |column|
+        @column = column
+      end
 
-    values.all? do |value|
-      model[@column] = value
-      model.valid?
+      description do
+        "be valid"
+      end
     end
-  end
-
-  chain :column do |column|
-    @column = column
-  end
-end
-
-RSpec::Matchers.define :invalid_all_values do |values|
-  match do |model|
-    return false if @column.blank?
-
-    values.none? do |value|
-      model[@column] = value
-      model.valid?
+    
+    RSpec::Matchers.define :invalid_all_values do |values|
+      match do |model|
+        return false if @column.blank?
+    
+        values.none? do |value|
+          model[@column] = value
+          model.valid?
+        end
+      end
+    
+      chain :column do |column|
+        @column = column
+      end
+      
+      description do
+        "not be valid"
+      end
     end
-  end
-
-  chain :column do |column|
-    @column = column
   end
 end
